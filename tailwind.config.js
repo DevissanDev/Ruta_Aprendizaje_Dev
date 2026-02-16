9ab130e9d0ea81e1a1dfc703d3cfe9b02c601cf8
deivymendoza <deivy.santiago@amarusuite.com>
2026-02-15 20:18:50 -0500

    init commit config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#000000",
+          100: "#f1faee",
+          200: "#457b9d",
+          300: "#a8dadc",
+          400: "#1d3557",
+          500: "#e63946",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
